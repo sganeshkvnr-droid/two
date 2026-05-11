144ea9d384a4f0ee55b7d3dc7982af76e2d04093
sganeshkvnr-droid <sganeshkvnr@gmail.com>
2026-05-11 19:24:09 +0530

    new

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
-function login() {
-    let user = document.getElementById("username").value;
-    let pass = document.getElementById("password").value;
-
-    // Simple demo login (hardcoded)
-    if (user === "admin" && pass === "1234") {
-        localStorage.setItem("loggedIn", "true");
-        window.location.href = "dashboard.html";
-    } else {
-        document.getElementById("error").innerText = "Invalid credentials!";
-    }
-}
-
-function logout() {
-    localStorage.removeItem("loggedIn");
-    window.location.href = "index.html";
-}
-
-// Protect dashboard
-if (window.location.pathname.includes("dashboard.html")) {
-    if (!localStorage.getItem("loggedIn")) {
-        window.location.href = "index.html";
-    }
+function showMessage(){
+    document.getElementById("message").innerHTML =
+    "GitHub Actions Workflow Working Successfully!";
 }
